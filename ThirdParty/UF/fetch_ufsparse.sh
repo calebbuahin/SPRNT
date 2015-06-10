a04d2a064c382e884de7dac2fb5e503a9518dfdc
Caleb Buahin <calebgh@gmail.com>
2015-06-10 00:41:27 -0500

    Updated links for libraries
    It seems the original download links are dead. All libraries have been packaged into the new Suitesparse. It compiles now.
    http://faculty.cse.tamu.edu/davis/suitesparse.html

--- ThirdParty/UF/fetch_ufsparse.sh
+++ ThirdParty/UF/fetch_ufsparse.sh
@@ -6,37 +6,29 @@
 #       modify this script if necessary!
 
 echo " "
-echo "Fetching UMFPACK and KLU from U. Florida CISE webpage,"
+echo "Fetching SuiteSparse from TIM DAVIS U. Tamu webpage,"
 echo " may take a few minutes depending on the speed of the internet connection ...."
 echo " "
-if [ ! -f UMFPACK-5.1.0.tar.gz ]; then
-curl http://www.cise.ufl.edu/research/sparse/umfpack/UMFPACK-5.1.0.tar.gz -o UMFPACK-5.1.0.tar.gz
+if [ ! -f SuiteSparse-3.7.1.tar.gz ]; then
+curl http://faculty.cse.tamu.edu/davis/SuiteSparse/SuiteSparse-3.7.1.tar.gz -o SuiteSparse-3.7.1.tar.gz
 fi
-if [ ! -f AMD-2.2.1.tar.gz ]; then
-curl http://www.cise.ufl.edu/research/sparse/amd/AMD-2.2.1.tar.gz -o AMD-2.2.1.tar.gz
-fi
-if [ ! -f BTF-1.1.3.tar.gz ]; then
-curl http://www.cise.ufl.edu/research/sparse/btf/BTF-1.1.3.tar.gz -o BTF-1.1.3.tar.gz
-fi
-if [ ! -f COLAMD-2.7.4.tar.gz ]; then
-curl http://www.cise.ufl.edu/research/sparse/colamd/COLAMD-2.7.4.tar.gz -o COLAMD-2.7.4.tar.gz
-fi
-if [ ! -f KLU-1.1.4.tar.gz ]; then
-curl http://www.cise.ufl.edu/research/sparse/klu/KLU-1.1.4.tar.gz -o KLU-1.1.4.tar.gz
-fi
-if [ ! -f UFconfig-3.7.1.tar.gz ]; then
-curl http://www.cise.ufl.edu/research/sparse/UFconfig/UFconfig-3.7.1.tar.gz -o UFconfig-3.7.1.tar.gz
-fi
+
 
 echo " "
 echo "Expanding the source code ......"
 echo " "
-tar -xzvf UMFPACK-5.1.0.tar.gz  > /dev/null 2>&1
-tar -xzvf AMD-2.2.1.tar.gz > /dev/null 2>&1
-tar -xzvf BTF-1.1.3.tar.gz > /dev/null 2>&1
-tar -xzvf COLAMD-2.7.4.tar.gz  > /dev/null 2>&1 
-tar -xzvf KLU-1.1.4.tar.gz  > /dev/null 2>&1
-tar -xzvf UFconfig-3.7.1.tar.gz  > /dev/null 2>&1
+tar -xzvf SuiteSparse-3.7.1.tar.gz  > /dev/null 2>&1
+
+# the original UFconfig.mk doesn't quite work for me
+# use the hacked version
+# 
+
+echo " "
+echo "Moving contents to main folder ......"
+echo " "
+mv Makefile Makefile.temp
+mv -v SuiteSparse/* ./
+mv Makefile.temp Makefile
 
 # the original UFconfig.mk doesn't quite work for me
 # use the hacked version
